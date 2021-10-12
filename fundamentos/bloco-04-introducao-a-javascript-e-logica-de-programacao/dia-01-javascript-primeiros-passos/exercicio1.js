1ae9d03af56b9c0033c3526827e81aae0ebfa546
kaylanerss <kaylane25@hotmail.com>
2021-10-12 12:50:59 -0300

    segunda parte do fixando concluida

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/exercicio1.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/exercicio1.js
@@ -1,9 +1,32 @@
 const myName = 'Kaylane';
 const birthCity = "Guarulhos";
-birthCity = 'Taubaté'
+// birthCity = 'Taubaté'
 let birthYear = 2002;
 birthYear = 2030;
 
 console.log(myName);
 console.log(birthCity);
 console.log(birthYear);
+
+// teste para segunda parte do fixando aprendizagens
+
+let patientId = 50;
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+console.log (typeof patientId);
+console.log (typeof patientAge);
+
+// começo do exercicio da segunda parte 
+
+const base = 5
+let heigth = 8
+const area = base * heigth
+const perimeter = 2*base + 2*heigth
+
+console.log (area)
+console.log (perimeter)
