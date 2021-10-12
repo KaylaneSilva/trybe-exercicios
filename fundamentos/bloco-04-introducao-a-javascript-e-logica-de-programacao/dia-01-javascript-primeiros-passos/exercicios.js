1877b1e426703530a3171231c07f7193e82a3a15
kaylanerss <kaylane25@hotmail.com>
2021-10-12 17:33:16 -0300

    Parte 2 do exercicio concluida

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/exercicios.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/exercicios.js
@@ -1,19 +1,30 @@
-const a = 9;
-const b = 28;
+// const a = 9;
+// const b = 28;
 
-// parte 1
+// // parte 1
 
-const soma = a + b;
-console.log (soma)
+// const soma = a + b;
+// console.log (soma)
 
-const subtracao = a - b;
-console.log (subtracao)
+// const subtracao = a - b;
+// console.log (subtracao)
 
-const multiplicacao = a * b;
-console.log (multiplicacao)
+// const multiplicacao = a * b;
+// console.log (multiplicacao)
 
-const divisao = a / b;
-console.log (divisao)
+// const divisao = a / b;
+// console.log (divisao)
 
-const modulo = a % b;
-console.log (modulo)
+// const modulo = a % b;
+// console.log (modulo)
+
+// parte 2
+
+let c = 747;
+let d = 653;
+
+if (c > d) {
+  console.log("c é maior que d");
+} else {
+  console.log("d é maior que c");
+};
