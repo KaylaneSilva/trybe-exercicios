c29063980d6e9afe3e8f9bd8cb843f92622721e1
kaylanerss <kaylane25@hotmail.com>
2021-10-12 12:37:48 -0300

    Concluido primeiro fixando conteudo

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/exercicio1.js
@@ -0,0 +1,9 @@
+const myName = 'Kaylane';
+const birthCity = "Guarulhos";
+birthCity = 'Taubaté'
+let birthYear = 2002;
+birthYear = 2030;
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
