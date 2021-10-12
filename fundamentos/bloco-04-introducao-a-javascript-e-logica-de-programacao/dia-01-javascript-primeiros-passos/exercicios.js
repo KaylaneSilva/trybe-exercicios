1d01de816fc10155717fb86ecb47f5212bd6c1dc
kaylanerss <kaylane25@hotmail.com>
2021-10-12 17:21:23 -0300

    Parte 1 do exercicio concluida

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/exercicios.js
@@ -0,0 +1,19 @@
+const a = 9;
+const b = 28;
+
+// parte 1
+
+const soma = a + b;
+console.log (soma)
+
+const subtracao = a - b;
+console.log (subtracao)
+
+const multiplicacao = a * b;
+console.log (multiplicacao)
+
+const divisao = a / b;
+console.log (divisao)
+
+const modulo = a % b;
+console.log (modulo)
