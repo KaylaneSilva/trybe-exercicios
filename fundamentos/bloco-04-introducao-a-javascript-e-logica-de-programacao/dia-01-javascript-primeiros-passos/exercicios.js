44b4cd19b8a01145dd1cc3af0f2f77c61b8da445
kaylanerss <kaylane25@hotmail.com>
2021-10-12 17:40:35 -0300

    Parte 3 do exercicio concluida

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/exercicios.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/exercicios.js
@@ -20,11 +20,25 @@
 
 // parte 2
 
-let c = 747;
-let d = 653;
+// let c = 747;
+// let d = 653;
 
-if (c > d) {
-  console.log("c é maior que d");
+// if (c > d) {
+//   console.log("c é maior que d");
+// } else {
+//   console.log("d é maior que c");
+// };
+
+// parte 3
+
+const e = 738;
+const f = 398;
+const g = 947;
+
+if ( e > f && e > g) {
+console.log ("'e' é maior que 'f' e 'g'")
+} else if (f > e && f > g) {
+  console.log ("'f' é maior que 'e' e 'g'")
 } else {
-  console.log("d é maior que c");
-};
+  console.log ("'g' é maior 'e' e 'f'")
+}
