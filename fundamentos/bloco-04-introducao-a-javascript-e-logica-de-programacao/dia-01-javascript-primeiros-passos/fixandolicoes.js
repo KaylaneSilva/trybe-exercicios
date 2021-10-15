a887608d00fcc47c692e3a1a634d3f6ddd84dcb3
kaylanerss <kaylane25@hotmail.com>
2021-10-14 21:43:52 -0300

    Resolvido exercicio 4 parte 1 de fixando aprendizagem

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/fixandolicoes.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/fixandolicoes.js
@@ -25,23 +25,43 @@ const patientEmail = 'ana@email.com';
 
 // exercicio 2 
 
-const base = 5
-let heigth = 8
+const base = 5;
+let heigth = 8;
 
-const area = base * heigth
-const perimeter = 2*base + 2*heigth
+const area = base * heigth;
+const perimeter = 2*base + 2*heigth;
 
 // console.log (area)
 // console.log (perimeter)
 
 // exercicio 3
 
-const notaDesafio = 50
+const notaDesafio = 50;
 
 if(notaDesafio >= 80){
-  console.log ('Parabéns!! Você foi aprovada(o)!')
+  // console.log ('Parabéns!! Você foi aprovada(o)!')
 }else if(notaDesafio <= 80 && notaDesafio >= 60) {
-  console.log ('Você está na lista de espera')
+  // console.log ('Você está na lista de espera')
 }else {
-  console.log ('Você foi reprovada(o)')
+  // console.log ('Você foi reprovada(o)')
+};
+
+// exercicio 4
+
+// operador AND
+const currentHour = 12;
+let message = null;
+
+if(currentHour >= 22){
+  message = 'Não deveríamos comer nada, é hora de dormir'
+}else if (currentHour >= 18 && currentHour < 22){
+message = 'Rango da noite, vamos jantar :D'
+}else if(currentHour >= 14 && currentHour < 18){
+  message = 'Vamos fazer um bolo para o café da tarde?'
+}else if(currentHour >= 11 && currentHour <= 14) {
+  message = 'Hora do almoço!!!'
+}else {
+  message = 'Hmmm, cheiro de café recém passado'
 }
+
+console.log (message)
