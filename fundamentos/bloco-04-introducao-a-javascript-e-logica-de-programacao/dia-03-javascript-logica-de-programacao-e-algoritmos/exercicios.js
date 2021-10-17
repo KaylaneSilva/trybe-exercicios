6097a682c6ba1abc6d12599f319e2811b03d57d8
kaylanerss <kaylane25@hotmail.com>
2021-10-17 17:10:41 -0300

    Resolvido exercicio 1

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-javascript-logica-de-programacao-e-algoritmos/exercicios.js
@@ -0,0 +1,15 @@
+// parte 1
+
+let n = 5;
+let aster = '*';
+let inputLine = '';
+
+for (let index = 0; index < n; index += 1) {
+  inputLine = inputLine + aster;
+};
+for (let index = 0; index < n; index += 1) {
+  // console.log(inputLine)
+}
+
+// parte 2
+
