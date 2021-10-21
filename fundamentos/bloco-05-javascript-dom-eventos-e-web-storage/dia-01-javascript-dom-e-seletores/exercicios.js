f98d87d5e7c8a491ff69c4ea4211c754528e8200
kaylanerss <kaylane25@hotmail.com>
2021-10-20 21:46:21 -0300

    Parte 1 dos exercicios feito

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-01-javascript-dom-e-seletores/exercicios.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-01-javascript-dom-e-seletores/exercicios.js
@@ -3,10 +3,22 @@
         - document.getElementById()
         - document.getElementsByClassName()
         - document.getElementsByTagName()
- Crie uma função que mude o texto na tag <p> para uma descrição de como você se vê daqui a 2 anos. (Não gaste tempo pensando no texto e sim realizando o exercício)
- Crie uma função que mude a cor do quadrado amarelo para o verde da Trybe (rgb(76,164,109)).
- Crie uma função que mude a cor do quadrado vermelho para branco.
- Crie uma função que corrija o texto da tag <h1>.
- Crie uma função que modifique todo o texto da tag <p> para maiúsculo.
- Crie uma função que exiba o conteúdo de todas as tags <p> no console.
+ 1- Crie uma função que mude o texto na tag <p> para uma descrição de como você se vê daqui a 2 anos. (Não gaste tempo pensando no texto e sim realizando o exercício)
+ 2- Crie uma função que mude a cor do quadrado amarelo para o verde da Trybe (rgb(76,164,109)).
+ 3- Crie uma função que mude a cor do quadrado vermelho para branco.
+ 4- Crie uma função que corrija o texto da tag <h1>.
+ 5- Crie uma função que modifique todo o texto da tag <p> para maiúsculo.
+ 6- Crie uma função que exiba o conteúdo de todas as tags <p> no console.
         */
+
+ let paragrafo = document.getElementsByTagName('p');
+ let quadrado = document.getElementsByClassName('mainContent');
+ let textoh1 = document.getElementsByTagName('h1');
+ let paragrafoAll = document.getElementsByClassName('centerContent');
+
+//  parte 1
+ function mudandoOParagrafo (p) {
+   paragrafo[1].innerText = 'Me vejo trabalhando em uma empresa que eu goste e com minha carreira alvancada';
+   return paragrafo;
+ }
+ console.log(mudandoOParagrafo(paragrafo));
