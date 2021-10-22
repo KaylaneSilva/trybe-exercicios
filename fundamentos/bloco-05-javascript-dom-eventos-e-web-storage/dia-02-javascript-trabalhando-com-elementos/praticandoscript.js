78966d00e70bdf2ea1d9384b72b242628034ed51
kaylanerss <kaylane25@hotmail.com>
2021-10-21 23:39:53 -0300

    Resolvido exercicio 1 - Parte II

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-02-javascript-trabalhando-com-elementos/praticandoscript.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-02-javascript-trabalhando-com-elementos/praticandoscript.js
@@ -29,7 +29,7 @@ let acessarQuartoEUltimoFilho= document.getElementById('quartoEUltimoFilho')
 
 // exercicio 1
 let imprimirElementoOndeVoce = ondeVoceEsta.parentNode;
-console.log(imprimirElementoOndeVoce);
+// console.log(imprimirElementoOndeVoce);
 
 // exercicio 2
 let ondeVoceEstaParaPai = imprimirElementoOndeVoce.parentNode;  ondeVoceEstaParaPai.style.color = 'green';
@@ -39,20 +39,36 @@ acessarPrimeiroFilhoDoFilho.innerText = 'Olá, seja bem-vindo a essa página';
 
 // exercicio 4
 let primeiroFilhoAPartirPai = acessarPai.firstChild
-console.log(primeiroFilhoAPartirPai);
+// console.log(primeiroFilhoAPartirPai);
 
 // exercicio 5
-console.log(ondeVoceEsta.parentNode);
+// console.log(ondeVoceEsta.parentNode);
 
 // exercicio 6
 let textoSegundoFilho = ondeVoceEsta.nextSibling;
 // console.log(textoSegundoFilho);
 
 // exercicio 7
-console.log(ondeVoceEsta.nextElementSibling);
+// console.log(ondeVoceEsta.nextElementSibling);
 
 // exercicio 8
-console.log(acessarPai.nextElementSibling);
+// console.log(acessarPai.nextElementSibling);
 
 // extra
 acessarTerceiroFilho.innerText = 'Terceiro Filho Elemento';
+
+
+// Parte II
+
+/* 1 - Crie um irmão para elementoOndeVoceEsta .
+2 - Crie um filho para elementoOndeVoceEsta .
+3 - Crie um filho para primeiroFilhoDoFilho .
+4 - A partir desse filho criado, acesse terceiroFilho .*/
+
+// exercicio 1
+let criaNovoElemento = document.createElement('section');
+criaNovoElemento.id = 'irmaoOndeVoceEsta'
+acessarPai.appendChild(criaNovoElemento);
+let acessarIrmaoOndeVoceEsta = document.getElementById('irmaoOndeVoceEsta')
+  acessarIrmaoOndeVoceEsta.innerText = 'Criando um elemento irmão para o #elementoOndeVoceEsta';
+  acessarIrmaoOndeVoceEsta.style.color = 'blue'
