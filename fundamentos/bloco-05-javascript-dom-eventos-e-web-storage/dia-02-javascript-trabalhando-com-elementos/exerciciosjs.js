67b10081ea8f4985f56d9b1b059c43732a08a7e0
kaylanerss <kaylane25@hotmail.com>
2021-10-24 14:23:39 -0300

    Exercicio 1 resolvido

---
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-02-javascript-trabalhando-com-elementos/exerciciosjs.js
@@ -0,0 +1,6 @@
+// exercicio 1
+let acessarBody = document.body;
+  let novaTag = document.createElement('h1');
+  novaTag.id = 'tirulo1';
+  novaTag.innerText = 'Exercício 5.2 - JavaScript DOM';
+  acessarBody.appendChild(novaTag);
