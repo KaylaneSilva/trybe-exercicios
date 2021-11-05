96d4fd2441791d5a039f031ff1b793a2cdb17566
kaylanerss <kaylane25@hotmail.com>
2021-11-04 22:51:29 -0300

    Começando parte 2 do exercicio

---
+++ fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-02-bibliotecas/script.js
@@ -0,0 +1,20 @@
+// let picker = new Pikaday({ field: $('#datepicker')[0] });
+
+// let picker = new Pikaday({
+//   field: document.getElementById('datepicker'),
+//   format: 'DD/MM/YYYY',
+//   toString(date, format) {
+//       const day = date.getDate();
+//       const month = date.getMonth() + 1;
+//       const year = date.getFullYear();
+//       return `${day}/${month}/${year}`;
+//   },
+//   parse(dateString, format) {
+//     // dateString is the result of `toString` method
+//     const parts = dateString.split('/');
+//     const day = parseInt(parts[0], 10);
+//     const month = parseInt(parts[1], 10) - 1;
+//     const year = parseInt(parts[2], 10);
+//     return;
+//   }
+// });
