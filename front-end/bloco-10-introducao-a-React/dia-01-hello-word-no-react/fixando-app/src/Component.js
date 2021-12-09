061303c8c201329465cc51d9805dee7d55f8b5f6
kaylanerss <kaylane25@hotmail.com>
2021-12-09 15:23:24 -0300

    exercicio fixanod aprendizado concluido

---
+++ front-end/bloco-10-introducao-a-React/dia-01-hello-word-no-react/fixando-app/src/Component.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Component extends React.Component {
+  render() {
+    return <div>
+      <h1>Kaylane Silva</h1>
+      <p> Moro em Taubaté, tenho 19 anos e atualmente sou uma estudante de Desenvolvimento Web na Trybe.</p>
+    </div>
+  }
+}
+
+export default Component
