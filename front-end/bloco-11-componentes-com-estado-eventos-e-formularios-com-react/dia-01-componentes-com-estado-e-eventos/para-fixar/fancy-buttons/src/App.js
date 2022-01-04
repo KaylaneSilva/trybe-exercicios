545aed77db66fe237a515199223ac2b76bef32eb
kaylanerss <kaylane25@hotmail.com>
2022-01-04 16:49:33 -0300

    exercicios de 'unindo componentes com estados e eventos' resolvido

--- front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-01-componentes-com-estado-e-eventos/para-fixar/fancy-buttons/src/App.js
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-01-componentes-com-estado-e-eventos/para-fixar/fancy-buttons/src/App.js
@@ -6,29 +6,27 @@ class App extends React.Component {
   constructor() {
     super();
     this.handleClickOne = this.handleClickOne.bind(this);
+
+    this.state = {
+      numeroDeCliques: 0
+    }
   }
 
-  handleClickOne () {
-    console.log(this);
-    console.log('Você clicou no botão 1');
-  }
-  
-  handleClickTwo = () => {
-    console.log(this);
-    console.log('Você clicou no botão 2');
-  }
-  
-  handleClickThree = () => {
-    console.log('Você clicou no botão 3');
+  handleClickOne (numero) {
+    this.setState((estadoAnterior, _props) => ({
+      numeroDeCliques: estadoAnterior.numeroDeCliques + 1
+    }))
+    console.log(`Você clicou no botão ${numero}`);
   }
 
   render() {
     return (
       <div>
         <h1>Hello World</h1>
-        <button onClick={this.handleClickOne}>Botão 1</button>
-        <button onClick={this.handleClickTwo}>Botão 2</button>
-        <button onClick={this.handleClickThree}>Botão 3</button>
+        <button onClick={() => {this.handleClickOne('1')}}>Botão 1</button>
+        <button onClick={() => {this.handleClickOne('2')}}>Botão 2</button>
+        <button onClick={() => {this.handleClickOne('3')}}>Botão 3</button>
+        <h4>Número de cliques: {this.state.numeroDeCliques}</h4>
       </div>
     )
   }
