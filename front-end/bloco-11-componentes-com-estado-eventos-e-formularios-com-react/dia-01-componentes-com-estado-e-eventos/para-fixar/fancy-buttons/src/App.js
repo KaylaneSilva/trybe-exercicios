ab344faac611c854a4cb9de5b87ca86d691cd407
kaylanerss <kaylane25@hotmail.com>
2022-01-04 15:29:28 -0300

    Exercicio de 'introdução de eventos' resolvido

--- front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-01-componentes-com-estado-e-eventos/para-fixar/fancy-buttons/src/App.js
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-01-componentes-com-estado-e-eventos/para-fixar/fancy-buttons/src/App.js
@@ -1,9 +1,27 @@
 import React from 'react';
 import './App.css';
 
+const handleClickOne = () => {
+  console.log('Você clicou no botão 1')
+}
+
+const handleClickTwo = () => {
+  console.log('Você clicou no botão 2')
+}
+
+const handleClickThree = () => {
+  console.log('Você clicou no botão 3')
+}
 class App extends React.Component {
   render() {
-    return <h1>Hello World</h1>
+    return (
+      <div>
+        <h1>Hello World</h1>
+        <button onClick={handleClickOne}>Botão 1</button>
+        <button onClick={handleClickTwo}>Botão 2</button>
+        <button onClick={handleClickThree}>Botão 3</button>
+      </div>
+    )
   }
 }
 
