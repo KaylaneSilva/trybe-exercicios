3c2d8044d34b9ffd36a5133e45b56d3fcbc99347
kaylanerss <kaylane25@hotmail.com>
2022-01-04 16:12:50 -0300

    Exercicio de "vinculando funcoes as classes..." resolvido

--- front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-01-componentes-com-estado-e-eventos/para-fixar/fancy-buttons/src/App.js
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-01-componentes-com-estado-e-eventos/para-fixar/fancy-buttons/src/App.js
@@ -1,25 +1,34 @@
 import React from 'react';
 import './App.css';
 
-const handleClickOne = () => {
-  console.log('Você clicou no botão 1')
-}
 
-const handleClickTwo = () => {
-  console.log('Você clicou no botão 2')
-}
-
-const handleClickThree = () => {
-  console.log('Você clicou no botão 3')
-}
 class App extends React.Component {
+  constructor() {
+    super();
+    this.handleClickOne = this.handleClickOne.bind(this);
+  }
+
+  handleClickOne () {
+    console.log(this);
+    console.log('Você clicou no botão 1');
+  }
+  
+  handleClickTwo = () => {
+    console.log(this);
+    console.log('Você clicou no botão 2');
+  }
+  
+  handleClickThree = () => {
+    console.log('Você clicou no botão 3');
+  }
+
   render() {
     return (
       <div>
         <h1>Hello World</h1>
-        <button onClick={handleClickOne}>Botão 1</button>
-        <button onClick={handleClickTwo}>Botão 2</button>
-        <button onClick={handleClickThree}>Botão 3</button>
+        <button onClick={this.handleClickOne}>Botão 1</button>
+        <button onClick={this.handleClickTwo}>Botão 2</button>
+        <button onClick={this.handleClickThree}>Botão 3</button>
       </div>
     )
   }
