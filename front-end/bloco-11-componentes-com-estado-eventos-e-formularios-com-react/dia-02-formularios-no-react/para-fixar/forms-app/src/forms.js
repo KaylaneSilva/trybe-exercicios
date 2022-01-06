597ac55f6a5512d48cc2ed1f44a78f27389b973a
kaylanerss <kaylane25@hotmail.com>
2022-01-06 15:18:58 -0300

    iniciando formulario

---
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-02-formularios-no-react/para-fixar/forms-app/src/forms.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class Forms extends React.Component {
+  render() {
+    return(
+      <section>
+        <h1> Formulário</h1>
+        <div>
+
+        </div>
+      </section>
+    )
+  }
+}
+
+export default Forms
