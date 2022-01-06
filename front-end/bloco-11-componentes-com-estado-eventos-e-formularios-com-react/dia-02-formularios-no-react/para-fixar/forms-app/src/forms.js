de1ef471bc6bbe567b409ad78615b02dc1d4a6b6
kaylanerss <kaylane25@hotmail.com>
2022-01-06 20:24:13 -0300

    exercicios 1 e 2 de fixacao resolvidos

--- front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-02-formularios-no-react/para-fixar/forms-app/src/forms.js
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-02-formularios-no-react/para-fixar/forms-app/src/forms.js
@@ -1,12 +1,66 @@
 import React from "react";
 
 class Forms extends React.Component {
+  constructor() {
+    super();
+    this.handleChange = this.handleChange.bind(this);
+
+    this.state = {
+      name: '',
+      termo: false,
+      regiao: '',
+      assunto: '',
+    };
+  }
+
+  handleChange(event) {
+    // const { target } = event;
+    this.setState({
+      name: event.target.value,
+    });
+  }
   render() {
-    return(
+    return (
       <section>
         <h1> Formulário</h1>
         <div>
-
+          <form className="forms">
+            <label>
+              Nome
+              <input
+                name="nome"
+                value={this.state.nome}
+                onChange={this.handleChange}
+              />
+            </label>
+            <label>
+              <input
+                type='checkbox'
+                name="termo"
+              // onChange={}
+              />
+              Você aceita os termos e diretrizes
+            </label>
+            <label>
+              Região
+              <select
+                name="regiao"
+              >
+                <option></option>
+                <option>Norte</option>
+                <option>Nordeste</option>
+                <option>Centro-Oeste</option>
+                <option>Sul</option>
+                <option>Sudeste</option>
+              </select>
+            </label>
+            <label>
+              Resume seu assunto
+              <textarea name='assunto'
+                maxLength={300}
+              ></textarea>
+            </label>
+          </form>
         </div>
       </section>
     )
