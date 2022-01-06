f6bec939f367a88aa45d52c7b3112818574c229c
kaylanerss <kaylane25@hotmail.com>
2022-01-06 20:54:24 -0300

    exercicio 6 de fixacao resolvido forms.js

--- front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-02-formularios-no-react/para-fixar/forms-app/src/forms.js
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-02-formularios-no-react/para-fixar/forms-app/src/forms.js
@@ -17,7 +17,7 @@ class Forms extends React.Component {
     const { name } = target;
     const value = target.type === 'checkbox' ? target.checked : target.value;
     this.setState({
-      [ name ]: value,
+      [name]: value,
     });
   }
   render() {
@@ -26,52 +26,59 @@ class Forms extends React.Component {
         <h1> Formulário</h1>
         <div>
           <form className="forms">
-            <label>
-              Nome
-              <input
-                name="nome"
-                value={this.state.nome}
-                onChange={this.handleChange}
-              />
-            </label>
-            <label>
-              <input
-                type='checkbox'
-                name="termo"
-                onChange={this.handleChange}
-              />
-              Você aceita os termos e diretrizes
-            </label>
-            <label>
-              Região
-              <select
-                name="regiao"
-                value={this.state.regiao}
-                onChange={this.handleChange}
-              >
-                <option></option>
-                <option>Norte</option>
-                <option>Nordeste</option>
-                <option>Centro-Oeste</option>
-                <option>Sul</option>
-                <option>Sudeste</option>
-              </select>
-            </label>
+            <fieldset>
+              <h4>Pré-Cadastro</h4>
+              <label>
+                Nome
+                <input
+                  className="div-inputs"
+                  name="nome"
+                  value={this.state.nome}
+                  onChange={this.handleChange}
+                />
+              </label>
+              <label>
+                <input
+                  type='checkbox'
+                  name="termo"
+                  onChange={this.handleChange}
+                />
+                Você aceita os termos e diretrizes
+              </label>
+              <label>
+                Região
+                <select
+                  className="div-inputs"
+                  name="regiao"
+                  value={this.state.regiao}
+                  onChange={this.handleChange}
+                >
+                  <option></option>
+                  <option>Norte</option>
+                  <option>Nordeste</option>
+                  <option>Centro-Oeste</option>
+                  <option>Sul</option>
+                  <option>Sudeste</option>
+                </select>
+              </label>
+              <label>
+                Coloque sua foto: 
+                <input
+                  className="div-inputs"
+                  name='arquivo'
+                  type='file'
+                />
+              </label>
+            </fieldset>
             <label>
               Resume seu assunto
-              <textarea 
+              <textarea
+                className="div-resume"
                 name='assunto'
                 maxLength={300}
                 value={this.state.assunto}
                 onChange={this.handleChange}
               ></textarea>
-            </label>
-            <label>
-              Coloque sua foto
-              <input
-                name='arquivo'
-                type='file'
-              />
             </label>
           </form>
         </div>
