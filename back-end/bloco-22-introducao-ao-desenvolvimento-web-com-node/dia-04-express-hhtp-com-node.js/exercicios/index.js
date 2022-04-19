627034d02c70f4a0c30bf6782dc82c5598f31c38
kaylanerss <kaylane25@hotmail.com>
2022-04-19 17:04:41 -0300

    exercicio 1 resolvido

---
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-node/dia-04-express-hhtp-com-node.js/exercicios/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.get('/ping', (_req, res) => {
+  res.json({ message: 'pong'});
+});
+
+app.listen(3001, () => {
+  console.log('Aplicação ouvindo na porta 3001');
+});
