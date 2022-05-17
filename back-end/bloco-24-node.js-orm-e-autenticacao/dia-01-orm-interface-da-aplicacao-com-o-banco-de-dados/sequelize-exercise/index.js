c8aa107c5774bb5ba9ac629029c9e60504f33234
kaylanerss <kaylane25@hotmail.com>
2022-05-17 18:47:52 -0300

    exercicio 5 resolvido: criando api para regastar todos os livros

--- back-end/bloco-24-node.js-orm-e-autenticacao/dia-01-orm-interface-da-aplicacao-com-o-banco-de-dados/sequelize-exercise/index.js
+++ back-end/bloco-24-node.js-orm-e-autenticacao/dia-01-orm-interface-da-aplicacao-com-o-banco-de-dados/sequelize-exercise/index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const router = require('./routes');
+
 app.use(express.json());
+
+app.use(router);
 
 app.listen(PORT, () => console.log(`Ouvindo na porta ${PORT}!`));
