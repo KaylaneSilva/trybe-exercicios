7e024cd2ffd95b934acc1be0ea6ed4beda9f5fb5
kaylanerss <kaylane25@hotmail.com>
2022-05-17 17:44:31 -0300

    exercício 1 resolvido: criando migration para a tabela Books

---
+++ back-end/bloco-24-node.js-orm-e-autenticacao/dia-01-orm-interface-da-aplicacao-com-o-banco-de-dados/sequelize-exercise/migrations/20220517203103-create-books.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Books', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      author: {
+        type: Sequelize.STRING
+      },
+      pageQuantity: {
+        type: Sequelize.STRING,
+        field: 'page_quantity'
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        field: 'created_at'
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        field: 'updated_at'
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Books');
+  }
+};
