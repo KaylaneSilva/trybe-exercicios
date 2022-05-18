93b0464ef98542976394293a859b910eeaa7af98
kaylanerss <kaylane25@hotmail.com>
2022-05-17 23:01:48 -0300

    exercicio 12 resolvido: criando função para atualizar um livro utilizando id no service

--- back-end/bloco-24-node.js-orm-e-autenticacao/dia-01-orm-interface-da-aplicacao-com-o-banco-de-dados/sequelize-exercise/services/bookService.js
+++ back-end/bloco-24-node.js-orm-e-autenticacao/dia-01-orm-interface-da-aplicacao-com-o-banco-de-dados/sequelize-exercise/services/bookService.js
@@ -12,12 +12,19 @@ const getById = async (id) => {
 
 const createBook = async (newBook) => {
   const createNewBook = await Book.create(newBook);
-  
+
   return createNewBook;
-}
+};
+
+const updateBook = async (id, book) => {
+ const [updatedBook] = await Book.update(book, { where: { id }});
+
+ return updatedBook;
+};
 
 module.exports = {
   getAll,
   getById,
   createBook,
+  updateBook
 };
