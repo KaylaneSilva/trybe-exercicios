c6194bfe071ac3ec8aab0dac480abc03f2a4eab8
kaylanerss <kaylane25@hotmail.com>
2022-05-17 23:11:03 -0300

    exercicio 17 resolvido: criando rota delete para deletar um livro

--- back-end/bloco-24-node.js-orm-e-autenticacao/dia-01-orm-interface-da-aplicacao-com-o-banco-de-dados/sequelize-exercise/routes/index.js
+++ back-end/bloco-24-node.js-orm-e-autenticacao/dia-01-orm-interface-da-aplicacao-com-o-banco-de-dados/sequelize-exercise/routes/index.js
@@ -10,4 +10,6 @@ router.post('/books', BookController.createBook);
 
 router.put('/books/:id', BookController.updateBook);
 
+router.delete('/books/:id', BookController.deleteBook);
+
 module.exports = router;
