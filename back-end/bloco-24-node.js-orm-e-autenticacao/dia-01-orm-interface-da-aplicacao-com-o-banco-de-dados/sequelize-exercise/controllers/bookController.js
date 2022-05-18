be07082c1742080a8e8ff2967eb012663131a34f
kaylanerss <kaylane25@hotmail.com>
2022-05-17 22:32:15 -0300

    exercicio 7 resolvido: criando função para buscar livro pelo id no controller

--- back-end/bloco-24-node.js-orm-e-autenticacao/dia-01-orm-interface-da-aplicacao-com-o-banco-de-dados/sequelize-exercise/controllers/bookController.js
+++ back-end/bloco-24-node.js-orm-e-autenticacao/dia-01-orm-interface-da-aplicacao-com-o-banco-de-dados/sequelize-exercise/controllers/bookController.js
@@ -6,6 +6,17 @@ const getAll = async (_req, res) => {
   return res.status(200).json(books);
 }
 
+const getById = async (req, res) => {
+  const { id } = req.params;
+
+  const getBook = await Book.getById(id);
+
+  if(!getBook) return res.status(404).json({ message: "Book not found"});
+
+  return res.status(200).json(getBook);
+};
+
 module.exports = {
   getAll,
+  getById,
 }
